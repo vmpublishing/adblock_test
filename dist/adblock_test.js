bb8bae74b6e898436ebb1568cff6cf1542b8dd28
Dirk Gustke <dirk.gustke@vmpublishing.com>
2017-08-01 16:00:03 +0200

    master - start the process after the document was loaded

--- dist/adblock_test.js
+++ dist/adblock_test.js
@@ -5,35 +5,38 @@ gs = gs || {};
 'use strict';
 
 // create test element and fill it with "evil" content
-gs.addblock_test_element = document.createElement('div');
-gs.addblock_test_element.innerHTML = '&nbsp;';
-gs.addblock_test_element.className = 'afs_ads';
 
-// attach it
-document.body.appendChild(gs.addblock_test_element);
+document.addEventListener('DOMContentLoaded', function() {
 
-// wait some time before the test,
-// so the add blockers will have a chance to notice the div
-setTimeout(function() {
+  var addblock_test_element = document.createElement('div');
+  addblock_test_element.innerHTML = '&nbsp;';
+  addblock_test_element.className = 'afs_ads';
 
-  // test for the add blockers effects
-  var text = '';
-  if (gs.addblock_test_element.offsetHeight === 0)
-    text = 'gs_addblocker_active';
-  else
-    text = 'gs_addblocker_inactive';
+  // attach it
+  document.body.appendChild(addblock_test_element);
 
-  // generate some debug information
-  if ('undefined' !== typeof(gs.debug))
-    console.log(text);
+  // wait some time before the test,
+  // so the add blockers will have a chance to notice the div
+  setTimeout(function() {
 
-  // throw the important events
-  var event = new Event(text);
-  document.dispatchEvent(event);
+    // test for the add blockers effects
+    var text = '';
+    if (addblock_test_element.offsetHeight === 0)
+      text = 'gs_addblocker_active';
+    else
+      text = 'gs_addblocker_inactive';
 
-  // remove our test-div
-  gs.addblock_test_element.remove();
-  gs.addblock_test_element = undefined;
+    // generate some debug information
+    if ('undefined' !== typeof(gs.debug))
+      console.log(text);
 
-}, 400);
+    // throw the important events
+    var event = new Event(text);
+    document.dispatchEvent(event);
 
+    // remove our test-div
+    addblock_test_element.remove();
+    addblock_test_element = undefined;
+
+  }, 400);
+});
